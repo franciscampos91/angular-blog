2a8c986f01ceb7c1da5f921643b1f68a2d38531f
Francis Campos <francis.goes@yahoo.com.br>
2024-02-15 21:12:50 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+    {
+        "id":"1",
+        "title": "NOVO HOMEM DE FERRO EM 3D",
+        "description": "Marvel Studios anuncia novo filme do Homem de Ferro",
+        "photoCover": "https://imageio.forbes.com/specials-images/imageserve/63600220295903240c876e05/0x0.jpg?format=jpg&height=600&width=1200&fit=bounds"
+    },
+    {
+        "id":"2",
+        "title": "Nova série anunciado no Disney +",
+        "description": "Bla bla bla",
+        "photoCover": "https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+    },
+    {
+        "id":"3",
+        "title": "Novo filme do Pantera Negra lançado em breve",
+        "description": "Bla bla bla",
+        "photoCover": "https://lumiere-a.akamaihd.net/v1/images/cg_disneyplus_blackpanther_mobile_1059_6488e33c.jpeg?region=0,0,1024,577&width=960"
+    },
+    {
+        "id":"4",
+        "title": "Novo filme dos X-Men está em pós produção, afirma diretor",
+        "description": "Bla bla bla bla 4",
+        "photoCover": "https://midias.correiobraziliense.com.br/_midias/jpg/2022/07/22/675x450/1_xmen-26096415.jpeg?20220722171339?20220722171339"
+    }
+]
